24130d74ee2c1cb8e18dcf8b1f91018dd4f7f86d
jeendale <jeekoo7203@gmail.com>
2023-12-09 03:06:01 +0900

    feat: UserDetailsImpl수정 ->getAuthorities 메서드 수정 AdminController Secured작동완료

--- src/main/java/com/example/springroadproject/controller/AdminController.java
+++ src/main/java/com/example/springroadproject/controller/AdminController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,14 +19,24 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
     private final PostService postService;
     //관리자 권한 게시글 전체 수정
-    @Secured(UserRoleEnum.Authority.ADMIN)
-    @PatchMapping("/admin/{id}")
-    public ResponseEntity<CommonResponseDto> admidUpdatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl user){
+/*    @Secured(UserRoleEnum.Authority.ADMIN)
+    @PostMapping("/admin}")
+    public ResponseEntity<CommonResponseDto> admidUpdatePost(@RequestBody PostRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl user){
         try{
-            postService.adminupdatePost(id,requestDto,user);
             return ResponseEntity.ok().body(new CommonResponseDto("관리자 수정", HttpStatus.OK.value()));
         }catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(new CommonResponseDto(e.getMessage(),HttpStatus.BAD_REQUEST.value()));
         }
+    }*/
+
+    @Secured(UserRoleEnum.Authority.ADMIN) // 관리자용 테스트
+    @GetMapping("/products/secured")
+    public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+        for (GrantedAuthority authority : userDetails.getAuthorities()) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
+
+        return "완료";
     }
 }
--- src/main/java/com/example/springroadproject/security/UserDetailsImpl.java
+++ src/main/java/com/example/springroadproject/security/UserDetailsImpl.java
@@ -1,10 +1,13 @@
 package com.example.springroadproject.security;
 
 import com.example.springroadproject.entity.User;
+import com.example.springroadproject.entity.UserRoleEnum;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class UserDetailsImpl implements UserDetails {
@@ -18,7 +21,14 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
     }
 
     @Override
